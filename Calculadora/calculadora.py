b71ad92fa238ba01e62aac36e6ecc8886be1718b
Carreira Fernando <fe.alves_carreira@hotmail.com>
2020-05-13 21:25:38 -0300

    adicionei mais algumas operações

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -10,6 +10,15 @@ def divisao(a, b):
 def multiplicacao(a, b):
     return a * b
 
+def potencia(a, b):
+    return a ** b
+
+def raiz(a, b):
+    return a ** (1 / b)
+
+def porcentagem(a, b):
+    return (a / 100) * b
+
 def escolha(valor):
     if o == '+':
         return soma(a, b)
@@ -19,13 +28,19 @@ def escolha(valor):
         return divisao(a, b)
     elif o == '*':
         return multiplicacao(a, b)
+    elif o == '^':
+        return a ** b
+    elif o == 'raiz':
+        return raiz(a, b)
+    elif o == '%':
+        return porcentagem(a, b)
 
 while True:
     a = float(input('Digite o primeiro valor: '))
     o = input('Qual a operação: ').strip().lower()
     b = float(input('Digite o segundo valor: '))
     resp = escolha(o)
-    print(f'= {resp}')
+    print('{}'.format(resp))
     saida = str(input('Quer sair: ')).strip().lower()
     if saida == 'sim':
         break
